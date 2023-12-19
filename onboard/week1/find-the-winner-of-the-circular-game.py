f91c401f15206d9339db419f2f681d8c2cbff933
Libeamlak W <95926254+libe12@users.noreply.github.com>
2023-12-19 11:14:02 -0800

    Added solution for Find the Winner of the Circular Game

--- onboard/week1/find-the-winner-of-the-circular-game.py
+++ onboard/week1/find-the-winner-of-the-circular-game.py
@@ -1,33 +1,11 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        if n == 1:
-            return 1
-        a = k
-        arr=[]
-        ans = [0]*n
-        for i in range(1,n+1):
-            arr.append(i)
-        freq = 0
-        r = 0
-        while True:
-            if r == len(arr):
-                r = 0
-            if ans[r] == 1:
-                r+=1
-                continue
-            a-=1
-            if a == 0:
-                
-                ans[r]=1
-                freq += 1
-                a=k
-            if freq == len(arr)-1:
-                break
-            r+=1
-            
-        for num in range(n):
-            if ans[num]==0:
-                return arr[num]
+        nums = list(range(n)) 
+        p = 0
+        while len(nums) > 1:
+            p = (p+k-1)%len(nums)
+            nums.pop(p)
+        return nums[0]+1
             
 
         
