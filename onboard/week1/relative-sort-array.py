3c715590bc5c206ccf78b01279ba1437991f67a8
Libeamlak W <95926254+libe12@users.noreply.github.com>
2023-12-27 06:24:51 -0800

    Added solution for Relative Sort Array

--- onboard/week1/relative-sort-array.py
+++ onboard/week1/relative-sort-array.py
@@ -1,22 +1,9 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        di = defaultdict(list)
-        for i,num in enumerate(arr1):
-            di[num].append(num)
-        
-        d2  = defaultdict(list)
-        for i, num in enumerate(arr2):
-            d2[num] = i
-        
-        arr = []
-        add = []
+        l = len(arr2)
+        d = defaultdict(lambda:l)
         for i,num in enumerate(arr2):
-            if num in di:
-                arr+=di[num]
-            
-        for num in di:
-            if num not in d2:
-                add+=di[num]
-        return arr+sorted(add)
+            d[num] = i
+        arr1.sort(key=lambda  x:(d[x],x))
 
-        
+        return arr1
