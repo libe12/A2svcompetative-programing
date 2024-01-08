cb791d4474ecaf04dc277a45c7f26d50e824cb3a
Libeamlak W <95926254+libe12@users.noreply.github.com>
2024-01-08 06:11:51 -0800

    Added solution for Maximum Number of Vowels in a Substring of Given Length

--- progress-sheet/maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ progress-sheet/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,21 +1,22 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
         
-        l = r = 0
-        ans = res = 0
-        st = 'aeiou'
-        while r < len(s):
-            if r-l+1 <=k:
-                if s[r] in st:
-                    ans += 1
-                r+=1
-                
-            else:
-                if s[l] in st:
-                    ans-=1
-                l+=1
-                
-            res = max(res,ans)
-                
-        return min(res,k)
-        
+        l =  0
+        intial = 0
+        vowel= 'aeiou'
+
+        for r in range(len(s[:k])):
+            if s[r] in vowel:
+                intial += 1
+
+        updated = intial
+        for r in range(k,len(s)):
+            if s[l] in vowel:
+                intial -=1
+            if s[r] in vowel:
+                intial +=1
+
+            if intial > updated:
+                updated = intial
+            l +=1
+        return updated
