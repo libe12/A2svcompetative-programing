632543845aa66e67b26ca23b8fe817b35c15549c
Libeamlak W <95926254+libe12@users.noreply.github.com>
2024-01-18 07:51:49 -0800

    Added solution for Product of Array Except Self

--- sliding_windows/product-of-array-except-self.py
+++ sliding_windows/product-of-array-except-self.py
@@ -1,19 +1,15 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        c = Counter(nums)
-        res = 1
-        for num in nums:
-            if num!=0:
-                res *= num
+        arr = [1]*len(nums)
+        for i in range(1,len(nums)):
+            arr[i] = nums[i-1]*arr[i-1]
+        res = [1]*len(nums)
+        post = 1
+        for i in range(len(nums)-1,-1,-1):
+            res[i] *= post
+            post *= nums[i] 
+            
         ans = []
-        for i, num in enumerate(nums):
-            if num ==0 and c[num] == 1:
-                ans .append(res)
-            elif num ==0 and c[num] > 1:
-                ans.append(0)
-            elif num !=0 and 0 not in c:
-                ans.append(res//num)
-            elif num !=0 and 0 in c:
-                ans.append(0)
+        for i in range(len(res)):
+            ans.append(arr[i]*res[i])
         return ans
-
